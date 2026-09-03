d1ed884baa9970085620cc685308882817259504
Pavan <abhintr13@gmail.com>
2026-09-03 09:19:57 +0530

    chore: add backend server script and api proxy in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,16 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+      '/ws': {
+        target: 'ws://localhost:8000',
+        ws: true,
+      },
+    },
   },
 });
+
